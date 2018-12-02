25d6a66e588eb5cc0c5ee64d0cbb21de29e7f715
Jake White <squirrelboy1225@gmail.com>
2018-12-01 23:15:21 -0500

    ope

--- car.py
+++ car.py
@@ -2,57 +2,62 @@ import pygame, sys
 from pygame.locals import *
 
 class Car(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x, y, width, height):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("car.png")
-        #self.rect = self.image.get_rect()
-        #self.rect.center = (screenWidth * .5, screenHeight * .75)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
         #fixing it to not move but rather just change values for a centered camera
-        self.x = screenWidth * .5
-        self.y = screenHeight * .75
+        self.x = x
+        self.y = y
         self.speed = 0
         self.speedX = 0
         self.minSpeed = 0
         self.maxSpeed = 10
-        self.minSpeedX = -1
-        self.maxSpeedX = 1
+        self.minSpeedX = -20
+        self.maxSpeedX = 20
+        self.screenWidth = width
+        self.screenHeight = height
 
     def apply_drag(self):
-        self.speed *= .99 
+        self.speed *= .95
+        self.speedX *= .9
     
     def physics(self):
-        print(self.speedX)
-        if(self.speedX > 0):
-            self.speedX -= 0.01
-        if(self.speedX < 0):
-            self.speedX += 0.01
-        print(self.speedX)
-        car.x += self.speedX
-        if car.x > screenWidth:
-            car.x = screenWidth
-        if car.x < 0:
-            car.x = 0
+        self.x += self.speedX
 
-    def check_input(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            car.speed += .5
-            if car.speed > car.maxSpeed:
-                car.speed = car.maxSpeed
-        elif keys[pygame.K_s]:
-            car.speed -= 1
-            if car.speed < car.minSpeed:
-                car.speed = car.minSpeed
-        if keys[pygame.K_d] and car.speed > 0:
-            car.speedX += 0.1
-            if(car.speedX > car.maxSpeedX):
-                car.speedX = car.maxSpeedX
-        elif keys[pygame.K_a] and car.speed > 0:
-            car.speedX -= 0.1
-            if(car.speedX < car.minSpeedX):
-                car.speedX = car.minSpeedX
+        self.rect.x = self.x
+        
+        #if self.x > self.screenWidth:
+        #    self.x = self.screenWidth
+        #if self.x < 0:
+        #    self.x = 0
+
+    def accelerate(self):
+        self.speed += .5
+        if self.speed > self.maxSpeed:
+            self.speed = self.maxSpeed
+    
+    def decelerate(self):
+        car.speed -= 1
+        if self.speed < self.minSpeed:
+            self.speed = self.minSpeed
+
+    def left(self):
+        if(self.speed > 0):
+            self.speedX -= 0.5
+            if(self.speedX > self.maxSpeedX):
+                self.speedX = self.maxSpeedX
+    
+    def right(self):
+        if(self.speed > 0):
+            self.speedX += 0.5
+            if(self.speedX < self.minSpeedX):
+                self.speedX = self.minSpeedX
 
     def update(self):
         self.physics()
-        self.check_input()
         self.apply_drag()
+
+    def getX(self):
+        return self.x
--- game.py
+++ game.py
@@ -1,28 +1,150 @@
 #game.py
 #mostly game loop code here
-import time
+import time, math
 import pygame, sys
+from car import Car
 
 class Game:
     def __init__(self, canvas_width, canvas_height):
+        #initializing pygame and the screen
+        pygame.init()
+        pygame.key.set_repeat(1, 10)
+        pygame.font.init()
+        self.myfont = pygame.font.SysFont('Courier New', 30)
         self.width = canvas_width
         self.height = canvas_height
+        self.screen = pygame.display.set_mode((self.width, self.height))
+
+        #setting up some road stuff
+        self.leftTurns = [60]
+        self.rightTurns = [15]
+        self.turnLength = 20
+        self.turnIntensity = 1
+        self.ease = 0
+        self.easeIn = False
+        self.roadRatio = 0.6
+        self.roadColor = pygame.Color(50, 50, 50)
+
+        #player stuff
+        self.drawdistance = 30
+        self.playerPos = 0
+        self.car = Car(self.width*0.5, self.height*0.85, self.width, self.height)
+        self.allSprites = pygame.sprite.Group(self.car)
     def start(self, fps):
         #start the game loop given frames per second
         self.millisBetweenTicks = (1/fps) * 1000
         self.lastTick = int(time.time() * 1000)
-        self.tick()
+        self.gameloop()
     def createscreen(self):        
-        return pygame.display.set_mode((self.width, self.height))
-    def tick(self):
+        return 
+    def gameloop(self):
         #this is the game loop
         while(True):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: sys.exit()
             if(int(time.time()*1000) > self.lastTick + self.millisBetweenTicks):
                 self.lastTick = int(time.time() * 1000)
-                self.update()
-    def update(self):
-        #this should be overridden with custom game code that gets called every tick
-        pass
+                self.tick()
+    def tick(self):
+        #this gets called every tick
+        self.check_input()
+        self.drawBackground()
+        self.drawRoad()
+        self.movement()
+        
+        self.allSprites.update()
+        self.allSprites.draw(self.screen)
+        pygame.display.update()
+
+    def drawRoad(self):
+        left=False
+        right=False
+        nextTurn = 0
+        roadWidth = self.width
+        stripeWidth = roadWidth/20
+        turnPos = 0
+        playerDistAway = 0
+        for i in range(0, len(self.leftTurns)):
+            if(self.playerPos > self.leftTurns[i] - self.drawdistance and self.playerPos < self.leftTurns[i] + self.turnLength):
+                left = True
+                nextTurn = i
+                playerDistAway = self.playerPos - self.leftTurns[nextTurn]
+        for i in range(0, len(self.rightTurns)):
+            if(self.playerPos > self.rightTurns[i] - self.drawdistance and self.playerPos < self.rightTurns[i] + self.turnLength):
+                right = True
+                nextTurn = i
+                playerDistAway = self.playerPos - self.rightTurns[nextTurn]
+
+        self.easeIn = playerDistAway < 0
+        if(left or right):
+            if(self.easeIn and self.ease <= self.turnIntensity):
+                self.ease += 0.1
+            elif(not self.easeIn and self.ease >= 0):
+                self.ease -= 0.1
+            if(self.ease < 0):
+                self.ease = 0
+        else:
+            self.ease = 0
+        for i in range(1, int(self.height*self.roadRatio), 2):
+            roadX = 0
+            stripeLeftX = 0
+            stripeRightX = 0
+            rectPos = self.playerPos + (i/(self.height*self.roadRatio))*self.drawdistance
+            stripeColor = pygame.Color(255, 255, 255)
+            grassColor = pygame.Color(50, 150, 50)
+            roadY = self.height - i
+            percentPerspective = i/(self.height*self.roadRatio)
+            stripeVar = math.sin(30*math.pow(0.5+percentPerspective, 4) + 20*self.playerPos)
+            grassVar = math.sin(6*math.pow(0.5+percentPerspective, 4) + 4*self.playerPos)
+            print("{}, {}".format(i, rectPos))
+            #print(stripeVar)
+            if(stripeVar > 0):
+                stripeColor = pygame.Color(255, 0, 0)
+            if(grassVar > 0):
+                grassColor = pygame.Color(50, 200, 50)
+            #print(rectPosition)
+            if(left or right):
+                turnPos = (i/(self.height*self.roadRatio))*self.ease
+            else:
+                turnPos = 0
+            if(left):
+                roadX = self.width/2 - roadWidth/2 - turnPos*i
+                stripeLeftX = self.width/2 - roadWidth/2 - turnPos*i - stripeWidth/2
+                stripeRightX = self.width/2 + roadWidth/2 - turnPos*i- stripeWidth/2
+            elif(right):
+                roadX = self.width/2 - roadWidth/2 + turnPos*i
+                stripeLeftX = self.width/2 - roadWidth/2 + turnPos*i - stripeWidth/2
+                stripeRightX = self.width/2 + roadWidth/2 + turnPos*i- stripeWidth/2
+            else: 
+                roadX = self.width/2 - roadWidth/2
+                stripeLeftX = self.width/2 - roadWidth/2 - stripeWidth/2
+                stripeRightX = self.width/2 + roadWidth/2 - stripeWidth/2
+
+            pygame.draw.rect(self.screen, grassColor, (0, roadY, self.width, 2))
+            pygame.draw.rect(self.screen, self.roadColor, (roadX, roadY, roadWidth, 2))
+            pygame.draw.rect(self.screen, stripeColor, (stripeLeftX, roadY, stripeWidth, 2))
+            pygame.draw.rect(self.screen, stripeColor, (stripeRightX, roadY, stripeWidth, 2))
+            roadWidth-=self.width* 0.005
+            stripeWidth = int(roadWidth/20)+1
+
+    
+    def check_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.car.accelerate()
+        elif keys[pygame.K_s]:
+            self.car.decelerate()
+        if keys[pygame.K_d]:
+            self.car.right()
+        elif keys[pygame.K_a]:
+            self.car.left()
+
+    def movement(self):
+        self.playerPos +=0.1
+        textsurface = self.myfont.render("{}".format(self.playerPos), False, (0, 0, 0))
+        self.screen.blit(textsurface,(0,0))
+
+    def drawBackground(self):
+        pygame.draw.rect(self.screen, pygame.Color("cyan"), (0,0,self.width,self.height*(1-self.roadRatio) + 1))
+
 
